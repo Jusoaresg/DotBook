8a29fb49f7596d87b30483bec6cac4f51ed0dc58
Jusoareg <julianosgreg@gmail.com>
2024-01-01 04:30:27 -0300

    Feat: added Product in IUnitOfWork

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace Bulky.DataAccess.Repository.IRepository
     public interface IUnitOfWork 
     {
         ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
 
         public void Save();
     }
