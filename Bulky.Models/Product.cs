52d3f7219298720ad8d7e7652becb3d17bd00026
Jusoareg <julianosgreg@gmail.com>
2024-01-01 16:51:47 -0300

    Feat: added CategoryId, Category and ImageUrl to Product Model

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Bulky.Models
@@ -37,5 +38,10 @@ namespace Bulky.Models
         [Range(1, 1000)]
         public double Price100 { get; set; }
 
+        public int CategoryId { get; set; }
+        [ForeignKey("ProductId")]
+        public Category Category { get; set; }
+        
+        public string ImageUrl { get; set; }
     }
 }
