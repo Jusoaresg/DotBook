94d74715d1e3ef8ef829b6409d5e5163aa8c6f39
Jusoareg <julianosgreg@gmail.com>
2024-01-07 14:14:54 -0300

    Feat: created the ApplicationUser Model with the IdentityUser

---
+++ Bulky.Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
+
+namespace Bulky.Models{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public int Name {get; set;}
+
+        public string? StreetAddress{get; set;}
+        public string? City {get; set;}
+        public string? State {get; set;}
+        public string? PostalCode {get; set;}
+    }
+}
+
+
