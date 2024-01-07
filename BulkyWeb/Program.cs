eb25fe3b7674dca53f9cee82ba48fd657f68b676
Jusoareg <julianosgreg@gmail.com>
2024-01-07 04:47:48 -0300

    Feat: added razor pages on the program.cs

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 
@@ -33,6 +33,8 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
