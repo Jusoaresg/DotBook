89ef4e9acb984c4ab96dd2ed74e162025f74c0a8
Jusoareg <julianosgreg@gmail.com>
2024-01-07 14:17:24 -0300

    Feat: created the const role string on the SD in Utility

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -8,5 +8,9 @@ namespace Bulky.Utility
 {
     public static class SD
     {
+        public const string Role_User_Cust = "Customer";
+        public const string Role_User_Comp = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
     }
 }
