87279513912286be456c1d2f9a035fbe7e8586bd
Jusoareg <julianosgreg@gmail.com>
2024-01-07 22:51:35 -0300

    Feat: changed the SD for role_customer and role_company

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -8,8 +8,8 @@ namespace Bulky.Utility
 {
     public static class SD
     {
-        public const string Role_User_Cust = "Customer";
-        public const string Role_User_Comp = "Company";
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
     }
