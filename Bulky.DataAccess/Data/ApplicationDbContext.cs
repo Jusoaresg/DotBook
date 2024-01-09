8293ed64c4cd48f7f69c311a2a02d0ad75c27a63
Jusoareg <julianosgreg@gmail.com>
2024-01-09 16:46:20 -0300

    Feat: added the ShoppinCartItem to the dbSet

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace Bulky.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers {get; set;}
+        public DbSet<ShoppingCartItem> ShoppingCartItems {get; set;}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) 
         {
