bc7d65989ffd970df6bb013c9e552c67fd0ed5c7
Jusoareg <julianosgreg@gmail.com>
2024-01-09 16:47:04 -0300

    Feat: created the ShoppingCartItemRepository

---
+++ Bulky.DataAccess/Repository/ShoppingCartItemRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Common;
+using System.Linq.Expressions;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess;
+
+public class ShoppingCartItemRepository : Repository<ShoppingCartItem>, IShoppingCartItem
+{
+    private readonly ApplicationDbContext _db;
+
+    public ShoppingCartItemRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public void Update(ShoppingCartItem obj)
+    {
+        //var itemFromDb = _db.ShoppingCartItems.FirstOrDefault(u=>u.Id==obj.Id);
+        _db.ShoppingCartItems.Update(obj);
+    }
+}
