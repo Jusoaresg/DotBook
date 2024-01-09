96352bcfb190097404cdf739b724d209963473ff
Jusoareg <julianosgreg@gmail.com>
2024-01-09 16:46:49 -0300

    Feat: created the ShoppingCartItem Repository Interface

---
+++ Bulky.DataAccess/Repository/IRepository/IShoppingCartItem.cs
@@ -0,0 +1,10 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+
+namespace Bulky.DataAccess;
+
+public interface IShoppingCartItem : IRepository<ShoppingCartItem>
+{
+    public void Update(ShoppingCartItem obj);
+}
