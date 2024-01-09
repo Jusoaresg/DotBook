552d2bc000cd6ddf328f58b1779d8b4eaac8ef29
Jusoareg <julianosgreg@gmail.com>
2024-01-09 16:46:32 -0300

    Feat: created the ShoppinCartController

---
+++ BulkyWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,32 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BulkyWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ShoppingCart : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCart(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
