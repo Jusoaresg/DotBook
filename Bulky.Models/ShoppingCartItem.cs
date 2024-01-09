aa1c7bf9c0dc17936071e38226a75e38889a7ff5
Jusoareg <julianosgreg@gmail.com>
2024-01-09 16:46:06 -0300

    Feat: created the ShoppingCartItem class

---
+++ Bulky.Models/ShoppingCartItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bulky.Models{
+
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int Id {get; set;}
+        [Required]
+        public string UserId {get; set;}
+        [Required]
+        public string ProductId {get; set;}
+        [ForeignKey("ProductId")]
+        public Product Product {get; set;}
+        [Range(1, 1000)]
+        public int Amount {get; set;} = 1;
+
+    }
+};
+
+
