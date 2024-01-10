68f24ca6936e9fb10a6de73b6e0e357686490d8d
Jusoareg <julianosgreg@gmail.com>
2024-01-10 20:02:05 -0300

    Feat: instanciating a ShoppingCartItem with db in the UnitOfWork

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -12,6 +12,7 @@ namespace Bulky.DataAccess.Repository
     {
         public ICategoryRepository Category { get; private set;}
         public IProductRepository Product { get; private set;}
+        public IShoppingCartItem ShoppingCartItem { get; private set;}
        
         private ApplicationDbContext _db;
 
@@ -20,6 +21,7 @@ namespace Bulky.DataAccess.Repository
             _db = db;
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
+            ShoppingCartItem = new ShoppingCartItemRepository(_db);
         }
 
         public void Save()
