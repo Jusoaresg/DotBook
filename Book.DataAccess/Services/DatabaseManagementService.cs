569b4d37e2b3720f42f4423fadb09159adb457d7
Jusoaresg <julianosgreg@gmail.com>
2024-01-26 22:55:24 -0300

    Feat: created a async task to try to connect to the database with time intervals

--- Book.DataAccess/Services/DatabaseManagementService.cs
+++ Book.DataAccess/Services/DatabaseManagementService.cs
@@ -7,11 +7,51 @@ namespace Book.DataAccess.Services;
 
 public static class DatabaseManagementService
 {
-    public static void MigrationInitialization(IApplicationBuilder app)
+   // public static void MigrationInitialization(IApplicationBuilder app)
+   // {
+   //     using (var serviceScope = app.ApplicationServices.CreateScope())
+   //     {
+   //         serviceScope.ServiceProvider.GetService<ApplicationDbContext>().Database.Migrate();
+   //     }
+   // }
+    public static async Task MigrationInitialization(IApplicationBuilder app)
     {
-        using (var serviceScope = app.ApplicationServices.CreateScope())
+
+
+        using(var serviceScope = app.ApplicationServices.CreateScope())
         {
-            serviceScope.ServiceProvider.GetService<ApplicationDbContext>().Database.Migrate();
+            var dbContext = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+
+            int retyDelayMiliseconds = 1000;
+
+            int retries = 0;
+            int maxRetries = 5;
+
+            bool connected = false;
+
+            while(!connected && retries < maxRetries)
+            {
+                try
+                {
+                    if(dbContext.Database.CanConnect())
+                    {
+                        await dbContext.Database.MigrateAsync();
+                        connected = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error on connection to the database");
+                    retries++;
+                    await Task.Delay(retyDelayMiliseconds);
+                }
+            }
+
+            if(!connected)
+            {
+                throw new InvalidOperationException("Cannot connect to the database");
+            }
         }
     }
+
 }
