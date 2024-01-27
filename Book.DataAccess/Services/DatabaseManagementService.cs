749426958810490a4c1ccc2d289308424707c608
Jusoaresg <julianosgreg@gmail.com>
2024-01-27 02:46:12 -0300

    Feat: removed commented code in the DatabaseManagement

--- Book.DataAccess/Services/DatabaseManagementService.cs
+++ Book.DataAccess/Services/DatabaseManagementService.cs
@@ -7,13 +7,6 @@ namespace Book.DataAccess.Services;
 
 public static class DatabaseManagementService
 {
-   // public static void MigrationInitialization(IApplicationBuilder app)
-   // {
-   //     using (var serviceScope = app.ApplicationServices.CreateScope())
-   //     {
-   //         serviceScope.ServiceProvider.GetService<ApplicationDbContext>().Database.Migrate();
-   //     }
-   // }
     public static async Task MigrationInitialization(IApplicationBuilder app)
     {
 
@@ -33,8 +26,10 @@ public static class DatabaseManagementService
             {
                 try
                 {
+                    Console.WriteLine("Tyring to connect to the database for migration");
                     if(dbContext.Database.CanConnect())
                     {
+                        Console.WriteLine("Tyring to make the migration on the db");
                         await dbContext.Database.MigrateAsync();
                         connected = true;
                     }
