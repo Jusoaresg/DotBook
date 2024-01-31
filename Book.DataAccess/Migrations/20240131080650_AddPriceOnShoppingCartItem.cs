b05bd20ba69dace36c1af0058d50eae042cf65aa
Jusoaresg <julianosgreg@gmail.com>
2024-01-31 05:48:46 -0300

    Feat: adding migration for the ShoppingCartItem Price

---
+++ Book.DataAccess/Migrations/20240131080650_AddPriceOnShoppingCartItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Book.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceOnShoppingCartItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "ShoppingCartItems",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "ShoppingCartItems");
+        }
+    }
+}
