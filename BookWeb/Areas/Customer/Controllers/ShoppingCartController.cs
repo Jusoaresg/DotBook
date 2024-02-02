ef826fc0647f559bb95766913bf7d0465f192a59
Jusoaresg <julianosgreg@gmail.com>
2024-02-02 15:33:16 -0300

    Feat: add IConfiguration in ShoppinCartController to the StripeService

--- BookWeb/Areas/Customer/Controllers/ShoppingCartController.cs
+++ BookWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
+using Book.Utility;
 
 namespace BookWeb.Areas.Customer.Controllers
 {
@@ -14,13 +15,16 @@ namespace BookWeb.Areas.Customer.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IConfiguration _config;
 
         public ShoppingCart(
             ILogger<HomeController> logger,
-            IUnitOfWork unitOfWork)
+            IUnitOfWork unitOfWork,
+            IConfiguration config)
         {
             _logger = logger;
             _unitOfWork = unitOfWork;
+            _config = config;
         }
 
 
@@ -30,6 +34,19 @@ namespace BookWeb.Areas.Customer.Controllers
             IEnumerable<ShoppingCartItem> shoppingCartItemList = await _unitOfWork.ShoppingCartItem.GetAllAsync(includeProperties: "Product");
             shoppingCartItemList = shoppingCartItemList.Where<ShoppingCartItem>(u => u.UserId==userId);
             return View(shoppingCartItemList);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IndexPost()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            IEnumerable<ShoppingCartItem> shoppingCartItemsList = await _unitOfWork.ShoppingCartItem.GetAllAsync(includeProperties: "Product");
+            shoppingCartItemsList = shoppingCartItemsList.Where<ShoppingCartItem>(u => u.UserId==userId);
+            var returnUrl = "http://localhost:5171/";
+            var StripeService = new StripeService(_config);
+            var session = StripeService.CreateCheckoutSession(shoppingCartItemsList, returnUrl);
+
+            return Redirect(session.Url);
         }
 
         [HttpDelete]
